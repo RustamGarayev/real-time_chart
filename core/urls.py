905516194f459a84d2414f88e3b604ca59bc4f98
Rustam Garayev <rustam.garayev.std@bhos.edu.az>
2022-03-17 04:12:11 +0400

    Some sample charts added

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ app_name = "core"
 
 urlpatterns = [
     path('', views.BaseIndexView.as_view(), name='index'),
+    path('chart/', views.FusionChartView.as_view(), name='chart'),
+    path('real-time/', views.RealTimeChartView.as_view(), name='real_time'),
 ]
